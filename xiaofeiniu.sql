e94ba0453f5ac583609849dafe727623e4f49290
liangtaotao <15114905254@163.com>
2019-01-25 15:38:28 +0800

    创建的数据库脚本文件

---
+++ xiaofeiniu.sql
@@ -0,0 +1,18 @@
+SET NAMES UTF8;
+DROP DATABASE IF EXISTS xiaofeiniu;
+CREATE DATABASE xiaofeiniu CHARSET=UTF8;
+USE xiaofeiniu;
+CREATE TABLE xfn_admin(
+  aid  INT PRIMARY KEY AUTO_INCREMENT,
+  aname VARCHAR(32),
+  apwd VARCHAR(64),
+);
+INSERT INTO xfn_admin VALUES
+  ();
+CREATE TABLE xfn_settings(
+  sid INT PRIMARY KEY AUTO_INCREMENT,
+
+)
+
+
+
